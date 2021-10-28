3af1766be25f6484fe49c501ea81f289fd7a9c17
Joakim Gunst <joakim.gunst@nitor.com>
2021-10-28 15:55:47 +0300

    Add initial app with tailwind

---
+++ components/HomeView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomeView() {
+  return (
+    <section className="h-screen flex items-center justify-center flex-col gap-10">
+      <h1 className="text-3xl select-none">Sustainable digital engineering</h1>
+    </section>
+  );
+}
