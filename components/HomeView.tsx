6588261538581d9a856d51e54a2cea9b54d9dd94
Joakim Gunst <joakim.gunst@nitor.com>
2021-10-29 09:24:48 +0300

    Update theme and styles

--- components/HomeView.tsx
+++ components/HomeView.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 export default function HomeView() {
+  function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+  }
+
   return (
-    <section className="h-screen flex items-center justify-center flex-col gap-10">
-      <h1 className="text-3xl select-none">Sustainable digital engineering</h1>
+    <section className="h-screen flex items-center justify-center flex-col gap-10 dark:bg-rockBlack">
+      <h1 className="text-3xl select-none text-neonBlue lg:text-6xl dark:text-malibuBlue">
+        Sustainable digital engineering
+      </h1>
+      <div className="flex items-center gap-4">
+        <button onClick={toggleDarkMode}>Toggle dark mode</button>
+        <button>Do nothing</button>
+      </div>
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
+const colors = {
+  rockBlack: "#26273A",
+  neonBlue: "#6056EB",
+  malibuBlue: "#6DC4E2",
+  pureWhite: "#FFFFFF",
+};
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
+    colors: {
+      rockBlack: "#26273A",
+      neonBlue: "#6056EB",
+      malibuBlue: "#6DC4E2",
+      pureWhite: "#FFFFFF",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
