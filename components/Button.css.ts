1a39145f40957153c1639be42d69982c700bcd0c
Joakim Gunst <joakim.gunst@nitor.com>
2021-11-13 18:14:17 +0200

    Use recipes api for button variants

--- components/Button.css.ts
+++ components/Button.css.ts
@@ -1,34 +1,34 @@
-import { style, styleVariants } from "@vanilla-extract/css";
+import { recipe, RecipeVariants } from "@vanilla-extract/recipes";
 import { vars } from "../styles/theme.css";
 
-const buttonBase = style({
-  fontFamily: vars.font.ui,
-  fontSize: vars.fontSize.md,
-  textAlign: "center",
-  padding: "8px 16px",
-  borderWidth: 2,
-  borderStyle: "solid",
-  borderColor: vars.color.text,
-  borderRadius: 2,
-  transition: "all .2s ease",
-  ":hover": {
-    transform: "scale(1.05)",
+export const button = recipe({
+  base: {
+    fontFamily: vars.font.ui,
+    fontSize: vars.fontSize.md,
+    textAlign: "center",
+    padding: "8px 16px",
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: vars.color.text,
+    borderRadius: 2,
+    transition: "all .2s ease",
+    ":hover": {
+      transform: "scale(1.05)",
+    },
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        background: vars.color.text,
+        color: vars.color.background,
+      },
+      secondary: {
+        background: vars.color.background,
+        color: vars.color.text,
+      },
+    },
   },
 });
 
-export const buttonVariants = styleVariants({
-  primary: [
-    buttonBase,
-    {
-      background: vars.color.text,
-      color: vars.color.background,
-    },
-  ],
-  secondary: [
-    buttonBase,
-    {
-      background: vars.color.background,
-      color: vars.color.text,
-    },
-  ],
-});
+export type ButtonVariants = RecipeVariants<typeof button>;
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,8 @@
 import { ButtonHTMLAttributes } from "react";
-import { buttonVariants } from "./Button.css";
+import { button, ButtonVariants } from "./Button.css";
 
-type ButtonVariant = "primary" | "secondary";
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & ButtonVariants;
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant: ButtonVariant;
-};
-
-export default function Button(props: Props) {
-  const { variant, className, ...otherProps } = props;
-  return <button className={buttonVariants[variant]} {...otherProps} />;
+export default function Button({ variant, ...otherProps }: Props) {
+  return <button className={button({ variant })} {...otherProps} />;
 }
