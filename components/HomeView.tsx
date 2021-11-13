c4f0e12f890ee394ae7eb53708139ce7679a3696
Joakim Gunst <joakim.gunst@nitor.com>
2021-11-13 16:35:27 +0200

    Add page title

--- components/HomeView.tsx
+++ components/HomeView.tsx
@@ -1,6 +1,7 @@
 import { darkMode } from "../styles/theme.css";
 import Button from "./Button";
 import { buttons, container, text } from "./HomeView.css";
+import Head from "next/head";
 
 export default function HomeView() {
   function toggleDarkMode() {
@@ -9,6 +10,9 @@ export default function HomeView() {
 
   return (
     <section className={container}>
+      <Head>
+        <title>vanilla-extract</title>
+      </Head>
       <h1 className={text}>Sustainable digital engineering</h1>
       <div className={buttons}>
         <Button variant="primary" onClick={toggleDarkMode}>
