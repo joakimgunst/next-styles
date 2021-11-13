ca2cd55f1da3d9654d7939615b759c7edc2bdfe6
Joakim Gunst <joakim.gunst@nitor.com>
2021-11-13 13:52:53 +0200

    Remove unused font size variables

--- stitches.config.ts
+++ stitches.config.ts
@@ -26,13 +26,8 @@ export const { styled, getCssText, keyframes } = createStitches({
       10: "2.5rem",
     },
     fontSizes: {
-      base: "1rem",
-      lg: "1.125rem",
       xl: "1.25rem",
-      "2xl": "1.5rem",
       "3xl": "1.875rem",
-      "4xl": "2.25rem",
-      "5xl": "3rem",
       "6xl": "3.75rem",
     },
   },
