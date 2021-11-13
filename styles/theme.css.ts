92bb14623c3bd52a2838956b2c56a71b2cbd6680
Joakim Gunst <joakim.gunst@nitor.com>
2021-11-13 13:52:43 +0200

    Remove unused font size variables

--- styles/theme.css.ts
+++ styles/theme.css.ts
@@ -32,13 +32,8 @@ const theme = {
     ui: "Roobert",
   },
   fontSize: {
-    base: "1rem",
-    lg: "1.125rem",
     xl: "1.25rem",
-    "2xl": "1.5rem",
     "3xl": "1.875rem",
-    "4xl": "2.25rem",
-    "5xl": "3rem",
     "6xl": "3.75rem",
   },
 };
