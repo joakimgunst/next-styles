f070414ed958e037a8ad7b4c7719d6cddfb8924f
Joakim Gunst <joakim.gunst@nitor.com>
2021-11-18 09:29:41 +0200

    Split global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import React from "react";
-import "../styles/global.css";
+import "../styles/reset.css";
+import "../styles/fonts.css";
+import "../styles/theme.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
